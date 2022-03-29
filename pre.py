c31a4de2d15eb8c1e0e2fa4b9d055d9d04033c56
mino0428 <894003870@qq.com>
2022-03-29 14:58:44 +0800

    update

--- pre.py
+++ pre.py
@@ -1,2 +1,8 @@
 from formatData import preData
-preData()
+from draw_topology import getOneNodePath, gene_topology_graphml
+
+fp = "./archive/Epoch.graphml"
+# 生成路径的邻接矩阵
+res, dist, n_node = gene_topology_graphml(fp)
+# 根据预存的APP和flows数据生成每条flow的path
+preData(res, dist, n_node)
